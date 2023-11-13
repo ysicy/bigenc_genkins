9ac6967743feb85747b5f05d2a2e69bc32a94aa9
ysicy <alexeyecko@gmail.com>
2023-11-13 15:46:28 +0300

    workinnn

--- src/test/java/BigencCatalogSearcWithJenkins.java
+++ src/test/java/BigencCatalogSearcWithJenkins.java
@@ -7,17 +7,28 @@ public class BigencCatalogSearcWithJenkins {
     private static final String URL = "https://bigenc.ru/";
     private  static final String ICON = "//span[contains(@class,'_categories')]";
     private static final String CATALOGUE = "//input[contains(@class,'b-search-input')]";
-    private static final String VALUE= "Биология";
+    private static final String VALUE1= "Биология";
+    private static final String VALUE2= "История";
     private static final String THEMATICPAGENAME= "//div[contains(@class,'bre-thematic-page')]";
 
 
     @Test
-    void voidSearchingTest() {
+    void categorySearchingTest() {
         open(URL);
         $x(ICON).click();
-        $x(CATALOGUE).setValue(VALUE).click();
+        $x(CATALOGUE).setValue(VALUE1).click();
         $x("//a[contains(@href,'t/biology')]").click();
-        $x(THEMATICPAGENAME).shouldHave(text(VALUE));
+        $x(THEMATICPAGENAME).shouldHave(text(VALUE1));
+    }
+
+    @Test
+    void categorySearching2Test(){
+        open(URL);
+        $x(ICON).click();
+        $x(CATALOGUE).setValue(VALUE2).click();
+        $x("//a[contains(@href,'t/history')]").click();
+        $x(THEMATICPAGENAME).shouldHave(text(VALUE2));
+
     }
 }
 

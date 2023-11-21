be0d9cabf42d20689615e6d03b8e94bf7f37c977
ysicy <alexeyecko@gmail.com>
2023-11-22 00:21:08 +0300

    workinnnn!!

--- src/test/java/BigencCatalogSearcWithJenkins.java
+++ src/test/java/BigencCatalogSearcWithJenkins.java
@@ -2,10 +2,11 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
-
+@Tag("simple")
 public class BigencCatalogSearcWithJenkins {
 
 
